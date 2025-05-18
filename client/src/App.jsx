3110363d2f484d4e59027eab202e04e62d623620
YulliErisna <167390598+YulliErisna@users.noreply.github.com>
2025-05-18 11:45:40 +0700

    App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import AddRecipe from './pages/AddRecipe';
-import RecipeDetail from './pages/RecipeDetail';
-import EditRecipe from './pages/EditRecipe';
-import NotFound from './pages/NotFound';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AddRecipe from "./pages/AddRecipe";
+import RecipeDetail from "./pages/RecipeDetail";
+import EditRecipe from "./pages/EditRecipe";
+import NotFound from "./pages/NotFound";
+import "./App.css"; // pastikan CSS diimpor
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/add" element={<AddRecipe />} />
-        <Route path="/edit/:id" element={<EditRecipe />} />
-        <Route path="/recipe/:id" element={<RecipeDetail />} />
-        <Route path="*" element={<NotFound />} />  {/* ini halaman 404 */}
-      </Routes>
-    </Router>
+    <div className="App">
+      {" "}
+      {/* Tambahkan wrapper dengan className="App" */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/add" element={<AddRecipe />} />
+          <Route path="/edit/:id" element={<EditRecipe />} />
+          <Route path="/recipe/:id" element={<RecipeDetail />} />
+          <Route path="*" element={<NotFound />} /> {/* ini halaman 404 */}
+        </Routes>
+      </Router>
+    </div>
   );
 }
+
 export default App;
